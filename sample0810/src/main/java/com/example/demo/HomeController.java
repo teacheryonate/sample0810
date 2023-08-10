8f20297cd419eff718c75cd6dc015f2ee3096f3e
a <a@naver.com>
2023-08-10 23:23:04 +0900

    집에서 작업

---
+++ sample0810/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mms")
+public class HomeController {
+	
+	@GetMapping("/")
+	public ResponseEntity<?> home(){
+		Map<String, String> map = new HashMap<>();
+		map.put("id", "moo1");
+		
+		return ResponseEntity.ok().body(map);
+	}
+
+}
